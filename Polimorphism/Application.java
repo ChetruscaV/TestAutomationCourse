ee6a678df4ee13b186a1deaf5e35d621f350e720
Chetrusca Victoria <chetruscav6@gmail.com>
2023-02-14 16:37:10 +0200

    Added new classes(polimorphism principle)

---
+++ Polimorphism/Application.java
@@ -0,0 +1,19 @@
+package Polimorphism;
+
+public class Application {
+    public static void main(String[] args) {
+  Animal pisicaMea=new Pisica();
+  pisicaMea.emiteSunet();
+
+  Animal ciineleNostru=new Ciine();
+  ciineleNostru.emiteSunet();
+
+
+
+
+
+
+
+
+    }
+}
